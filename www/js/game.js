a3d757befef243873a579c43f5a533500788f54d
Daniel Preciado <danielpre30@outlook.com>
2019-02-24 15:58:49 -0500

    Interfaz de carga y menu de inicio. Libreria de Phaser agregada

---
+++ www/js/game.js
@@ -0,0 +1,32 @@
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 360,
+//     height: 640,
+//     scene: {
+//         preload: preload,
+//         create: create,
+//         update: update
+//     }
+// };
+    
+
+// var game = new Phaser.Game(config);
+// var sky;
+
+// function preload ()
+// {
+//     this.load.image('logo','/img/logo.png');
+//     this.load.image('cargando','/img/elemento_cargando.png');
+    
+// }
+
+// function create ()
+// {
+//     this.cameras.main.setBackgroundColor("2e2e2e");
+//     this.add.image(180, 245, 'logo');
+//     this.add.image(180, 360, 'cargando');
+// }
+
+// function update ()
+// {
+// }
