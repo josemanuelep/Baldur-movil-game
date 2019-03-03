c21c7f7553ad124f1754ae2d48da6b9e623529e2
Daniel Preciado <danielpre30@outlook.com>
2019-03-02 22:37:01 -0500

    Pantalla de mapa, fondo del juego agregado y el personaje se puede mover. Un error no permite cargar las imagenes en el celular

--- www/js/game.js
+++ www/js/game.js
@@ -1,12 +1,18 @@
 var gameOver = false;
 var checkpoints = [false, false, false];
-var btnContinuar, btnOpciones, opcion_menu, opcion_personajes, opcion_musica, opcion_creditos, boton_volver;
+var btnContinuar, btnOpciones, opcion_menu, opcion_personajes, opcion_musica, opcion_creditos, btn_volver, btn_mapa ;
 
 var config = {
     type: Phaser.AUTO,
     width: 360,
     height: 640,
-    parent:"game",
+    parent:"pantalla_game",
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -16,18 +22,36 @@ var config = {
     
 
 var game = new Phaser.Game(config);
+var sprite;
 
 function preload ()
-{   cargarImagenes(this);
-    cargarBotones();btnContinuar
-    cambiarPantalla(pantalla_carga, pantalla_inicio);
+{   
+    cargarImagenes(this);
+    cargarBotones();
     asignarEventos();
+    cambiarPantalla(pantalla_carga, pantalla_inicio);
+}
+
+function cargarImagenes(game){
+    game.load.image('fondo_juego','../img/fondo_juego.gif');
+    game.load.image('asteroide_derecha_grande','../img/asteroide_derecha_grande.png');
+    game.load.image('dude','../img/personaje.png');
 }
 
 function create ()
 {
     
-    // this.add.image(180, 360, 'cargando');
+    this.add.image(180, 320, 'fondo_juego');
+    sprite = this.add.image(180, 560, 'dude');
+
+    this.physics.world.enable(sprite);
+    
+    sprite.body.setCollideWorldBounds(true);
+    sprite.setInteractive({ draggable: true });
+
+    sprite.on('drag', function (pointer, dragX, dragY) {
+        this.x = dragX;
+    });
 }
 
 function update ()
@@ -36,22 +60,20 @@ function update ()
 
 function asignarEventos(){
     asignarEventosInicio();
-    asignarEventosOpciones(); 
-}
-
-function cargarImagenes(game){
-    game.load.image('asteroide_derecha_grande','../img/asteroide_derecha_grande.png');
+    asignarEventosOpciones();
+    asignarEventoMapa();
 }
 
 function cargarBotones(){
     btnContinuar = document.getElementById("btnContinuar");
-    btnOpciones = document.getElementById("btnOpciones")
+    btnOpciones = document.getElementsByClassName("btnOpciones");
     btnMusica = document.getElementById("btnMusica");
     opcion_menu = document.getElementById("opcion_menu");
     opcion_personajes = document.getElementById("opcion_personajes");
     opcion_musica = document.getElementById("opcion_musica");
     opcion_creditos = document.getElementById("opcion_creditos");
-    boton_volver = document.getElementById("boton_volver");
+    btn_volver = document.getElementById("boton_volver");
+    btn_mapa = document.getElementById("boton_mapa");
 }
 
 function cambiarPantalla(origen, destino){
@@ -61,11 +83,14 @@ function cambiarPantalla(origen, destino){
 
 function asignarEventosInicio(){
     btnContinuar.addEventListener("click", function(){
-        console.log("Abrir Pantalla Mapa");
+        cambiarPantalla(pantalla_inicio, pantalla_mapa);
     });
-    btnOpciones.addEventListener("click", function(){
-        cambiarPantalla(pantalla_inicio, pantalla_opciones);
-    });
+
+    Array.from(btnOpciones).forEach(function(element) {
+        element.addEventListener('click', function(){
+            cambiarPantalla(element.parentElement, pantalla_opciones);
+        });
+      });
     btnMusica.addEventListener("click", function(){
         console.log("Apagar Musica");
     });
@@ -84,7 +109,13 @@ function asignarEventosOpciones(){
     opcion_creditos.addEventListener("click", function(){
         
     });
-    boton_volver.addEventListener("click", function(){
+    btn_volver.addEventListener("click", function(){
         cambiarPantalla(pantalla_opciones, pantalla_inicio);
+    });
+}
+
+function asignarEventoMapa(){
+    btn_mapa.addEventListener("click", function(){
+        cambiarPantalla(pantalla_mapa, pantalla_game);
     });
 }
